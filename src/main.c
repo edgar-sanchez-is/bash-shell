a2101559a43d2ed75744e5ef0f205099cc297ebc
Edgar Sanchez <edgr.sanchez@outlook.com>
2016-03-10 15:04:41 -0600

    Created initial main file and makefile
    CMakeLists is optional and supported by certain IDEs. It will not be
    included in final version.

---
+++ src/main.c
@@ -0,0 +1,22 @@
+// =====================================================================
+// Name:		Alex, Edgar, Luis, Russell
+// Course:		CSCE 3600
+// Date:		March 30, 2016
+// Title:		Major Assignment 1
+// Version:		1.0
+// Description:		Implement a command line interpreter or shell.
+// 			When a command is entered, the shell creates a child
+//			process that executes the command and then prompts for
+//			more input. Multiple commands can be separated by ';'
+// 			or by using a batch file.
+// Format:		shell [batchFile]
+// =====================================================================
+
+#include <stdio.h>
+
+int main () {
+
+	printf("Hello World!\n");
+
+	return 0;
+}
